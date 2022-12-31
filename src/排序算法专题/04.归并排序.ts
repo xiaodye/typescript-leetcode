bd7926441823e3880246c45ef3afff82cb9444a6
lansen <bluewoods.lan@gmail.com>
2022-12-31 21:38:26 +0800

    feat:归并&快排

---
+++ src/排序算法专题/04.归并排序.ts
@@ -0,0 +1,54 @@
+/**
+ * 归并排序
+ * @param arr 待排序数组
+ * @returns
+ */
+const mergeSort = function (arr: number[]): number[] {
+  const len = arr.length;
+
+  // 但被分割的数组只有一个元素时，返回数组。
+  if (len <= 1) return arr;
+  const mid = Math.floor(len / 2);
+  const leftArr = mergeSort(arr.slice(0, mid));
+  const rightArr = mergeSort(arr.slice(mid, len));
+  arr = mergeArr(leftArr, rightArr);
+
+  return arr;
+};
+
+/**
+ * 合并两个有序数组
+ * @param leftArr
+ * @param rightArr
+ * @returns
+ */
+const mergeArr = function (leftArr: number[], rightArr: number[]): number[] {
+  // 初始化两个指针，分别指向 arr1 和 arr2
+  let i = 0;
+  let j = 0;
+  const res: number[] = [];
+
+  while (i < leftArr.length && j < rightArr.length) {
+    if (leftArr[i] <= rightArr[j]) {
+      res.push(leftArr[i]);
+      i++;
+    } else {
+      res.push(rightArr[j]);
+      j++;
+    }
+  }
+
+  // 若其中一个子数组会首先被合并完全，则直接拼接另一个子数组的剩余部分
+  if (i < leftArr.length) {
+    return [...res, ...leftArr.slice(i)];
+  } else {
+    return [...res, ...rightArr.slice(j)];
+  }
+};
+
+// test
+const arr = [8, 7, 6, 5, 4, 3, 2, 1];
+console.log(mergeSort(arr));
+debugger;
+
+export {};
---
+++ src/排序算法专题/05.快速排序.ts
@@ -0,0 +1,61 @@
+/**
+ * 快速排序
+ * @param arr 待排序数组
+ * @param left
+ * @param right
+ * @returns
+ */
+const quickSort = function (arr: number[], left: number, right: number): number[] {
+  if (arr.length > 1) {
+    const pivotIndex = partition(arr, left, right);
+
+    if (left < pivotIndex - 1) {
+      quickSort(arr, left, pivotIndex - 1);
+    }
+    if (right > pivotIndex) {
+      quickSort(arr, pivotIndex, right);
+    }
+  }
+  return arr;
+};
+
+/**
+ * 以基准值为轴心，划分左右子数组的过程
+ * @param arr 待划分数组
+ * @param left
+ * @param right
+ * @returns
+ */
+const partition = function (arr: number[], left: number, right: number): number {
+  let pivot = arr[Math.floor(left + (right - left) / 2)];
+
+  // 初始化左右指针
+  let i = left;
+  let j = right;
+
+  while (i <= j) {
+    while (arr[i] < pivot) {
+      i++;
+    }
+    while (arr[j] > pivot) {
+      j--;
+    }
+
+    // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
+    if (i <= j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
+    }
+  }
+
+  // 返回左指针索引作为下一次划分左右子数组的依据
+  return i;
+};
+
+// test
+const arr = [5, 1, 3, 6, 2, 0, 7];
+console.log(quickSort(arr, 0, arr.length - 1));
+debugger;
+
+export {};
