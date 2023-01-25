45f4283edf3a1369046766501c3cca7d3e68eba3
lansen <bluewoods.lan@gmail.com>
2023-01-25 14:33:02 +0800

    feat: prettier

--- src/二叉树/236. 二叉树的最近公共祖先.ts
+++ src/二叉树/236. 二叉树的最近公共祖先.ts
@@ -7,7 +7,11 @@ import TreeNode from "../data-structure/TreeNode";
  * @param q
  * @returns
  */
-export default function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+export default function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
   // 若当前结点不存在（意味着无效）或者等于p/q（意味着找到目标），则直接返回
   if (!root || root === p || root === q) return root;
 
