2464a12c2103ed0f944ddf83436da9e9151be454
lansen <bluewoods.lan@gmail.com>
2023-01-29 12:29:46 +0800

    feat: 平衡二叉树

---
+++ src/二叉树/110. 平衡二叉树.ts
@@ -0,0 +1,28 @@
+import TreeNode from "../data-structure/TreeNode";
+
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * @param root
+ * @returns
+ */
+export default function isBalanced(root: TreeNode | null): boolean {
+  return height(root) !== -1;
+}
+
+/**
+ * 计算二叉树的高度，不平衡返回 -1
+ * @param root
+ * @returns
+ */
+function height(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  const leftHeight = height(root.left);
+  const rightHeight = height(root.right);
+
+  if (leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1) {
+    return -1;
+  }
+
+  return Math.max(leftHeight, rightHeight) + 1;
+}
---
+++ src/二叉树/1382. 将二叉搜索树变平衡.ts
@@ -0,0 +1,38 @@
+import TreeNode from "../data-structure/TreeNode";
+
+/**
+ * 给你一棵二叉搜索树，请你返回一棵 平衡后 的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。
+ * 如果有多种构造方法，请你返回任意一种。
+ * @param root
+ * @returns
+ */
+export default function balanceBST(root: TreeNode | null): TreeNode | null {
+  // nums 是二叉树中序遍历序列
+  const nums: number[] = [];
+
+  function inorder(root: TreeNode | null): void {
+    if (!root) return;
+
+    if (root.left) inorder(root.left);
+    nums.push(root.val);
+    if (root.right) inorder(root.right);
+  }
+
+  function buildAVL(left: number, right: number): TreeNode | null {
+    if (left > right) return null;
+
+    const mid = Math.floor(left + (right - left) / 2);
+    const root = new TreeNode(nums[mid]);
+
+    root.left = buildAVL(left, mid - 1);
+    root.right = buildAVL(mid + 1, right);
+
+    return root;
+  }
+
+  // 获取二叉树的中序遍历序列
+  inorder(root);
+
+  // 根据中序遍历序列构造平衡二叉搜索树
+  return buildAVL(0, nums.length - 1);
+}
