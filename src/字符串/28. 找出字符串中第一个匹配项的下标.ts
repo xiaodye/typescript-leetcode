a0830b04b3ae8688a05bf00379dcd38f819c813d
lansen <bluewoods.lan@gmail.com>
2023-02-24 21:11:21 +0800

    feat: 28

--- src/字符串/28. 找出字符串中第一个匹配项的下标.ts
+++ src/字符串/28. 找出字符串中第一个匹配项的下标.ts
@@ -2,20 +2,19 @@
  * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
  * 如果 needle 不是 haystack 的一部分，则返回  -1 。
  *
+ * - 时间复杂度：O(m+n)
+ * - 空间复杂度：O(m)
+ *
  * @param haystack
  * @param needle
  * @returns
  */
 export default function strStr(haystack: string, needle: string): number {
-  if (needle.length === 0) return 0;
-
   // 获取字符串needle的前缀表
   const next = getNext(needle);
 
   // i指向haystack, j指向needle
-  let j = 0;
-
-  for (let i = 0; i < haystack.length; i++) {
+  for (let i = 0, j = 0; i < haystack.length; i++) {
     // 不匹配，则回退
     while (j > 0 && haystack[i] !== needle[j]) {
       j = next[j - 1];
@@ -43,9 +42,8 @@ export default function strStr(haystack: string, needle: string): number {
 function getNext(s: string): number[] {
   const next = new Array<number>(s.length).fill(0);
 
-  let j = 0;
-
-  for (let i = 1; i < s.length; i++) {
+  // 定义两个指针i和j，j指向前缀末尾位置，i指向后缀末尾位置。
+  for (let i = 1, j = 0; i < s.length; i++) {
     while (j > 0 && s[i] !== s[j]) {
       j = next[j - 1];
     }
