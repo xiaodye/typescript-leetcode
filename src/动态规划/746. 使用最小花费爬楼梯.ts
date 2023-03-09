4c4602ca0985ff761cb636409e439e3b510b3fca
lansen <bluewoods.lan@gmail.com>
2023-03-09 23:52:09 +0800

    feat: 746

---
+++ src/动态规划/746. 使用最小花费爬楼梯.ts
@@ -0,0 +1,19 @@
+/**
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。
+ * 一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ *
+ * @param cost
+ * @returns
+ */
+export default function minCostClimbingStairs(cost: number[]): number {
+  // 初始化dp数组，dp[0] = 0, dp[1] = 0
+  const dp: number[] = [0, 0];
+
+  for (let i = 2; i <= cost.length; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+
+  return dp[cost.length];
+}
