50cfd4ee56fb3646e28451615db485216bb7a4ed
lansen <bluewoods.lan@gmail.com>
2023-03-17 13:47:06 +0800

    剑指 Offer: 14, 17

---
+++ src/剑指 Offer/14- I. 剪绳子.ts
@@ -0,0 +1,19 @@
+/**
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+ * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+ * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ * @param n
+ * @returns
+ */
+export default function cuttingRope(n: number): number {
+  const dp = new Array<number>(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j < i; j++) {
+      // dp[i] 表示将长度为 i 的绳子剪成至少两段绳子之后，这些绳子长度的最大乘积
+      dp[i] = Math.max(dp[i], j * (i - j), j * dp[i - j]);
+    }
+  }
+
+  return dp[n];
+}
---
+++ src/剑指 Offer/17. 打印从1到最大的n位数.ts
@@ -0,0 +1,14 @@
+/**
+ * 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+ * @param n
+ * @returns
+ */
+export default function printNumbers(n: number): number[] {
+  const res: number[] = [];
+
+  for (let i = 1; i < Math.pow(10, n); i++) {
+    res.push(i);
+  }
+
+  return res;
+}
