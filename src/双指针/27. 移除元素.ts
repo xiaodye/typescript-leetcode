415b3c27b02400a26bc0658cb68e3f8e25a54f07
lansen <bluewoods.lan@gmail.com>
2023-04-02 23:47:58 +0800

    双指针：27， 344

---
+++ src/双指针/27. 移除元素.ts
@@ -0,0 +1,20 @@
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * @param nums
+ * @param val
+ * @returns
+ */
+export default function removeElement(nums: number[], val: number): number {
+  let slow = 0;
+
+  for (let fast = 0; fast < nums.length; fast++) {
+    if (nums[fast] !== val) {
+      nums[slow] = nums[fast];
+      slow++;
+    }
+  }
+
+  return slow;
+}
---
+++ src/双指针/344. 反转字符串.ts
@@ -0,0 +1,12 @@
+/**
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * @param s
+ */
+export default function reverseString(s: string[]): void {
+  const len = s.length;
+
+  for (let i = 0, j = len - 1; i < j; i++, j--) {
+    [s[i], s[j]] = [s[j], s[i]];
+  }
+}
