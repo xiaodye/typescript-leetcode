0787bd9f7f8cdbd515712757ec3006bf726823fb
lansen <bluewoods.lan@gmail.com>
2023-04-05 20:59:09 +0800

    剑指 Offer: 58

---
+++ src/剑指 Offer/58 - II. 左旋转字符串.ts
@@ -0,0 +1,16 @@
+/**
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+ * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * @param s
+ * @param n
+ * @returns
+ */
+export default function reverseLeftWords(s: string, n: number): string {
+  const arr = [...s];
+
+  for (let i = 0; i < n; i++) {
+    arr.push(arr.shift());
+  }
+
+  return arr.join("");
+}
