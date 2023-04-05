ef1b6f3b7870ce6c4f9a3e3a1fc6a4a01838d5b3
lansen <bluewoods.lan@gmail.com>
2023-04-05 23:25:06 +0800

    feat: 225: 添加单个队列解法

--- src/栈与队列/225. 用队列实现栈.ts
+++ src/栈与队列/225. 用队列实现栈.ts
@@ -1,7 +1,39 @@
 /**
  * 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+ * 一个队列
  */
 export default class MyStack {
+  private queue: number[] = [];
+
+  constructor() {}
+
+  push(x: number): void {
+    this.queue.push(x);
+  }
+
+  pop(): number {
+    // 将队列头部的元素（除了最后一个元素外） 重新添加到队列尾部
+    for (let i = 0; i < this.queue.length - 1; i++) {
+      this.queue.push(this.queue.shift());
+    }
+
+    return this.queue.shift();
+  }
+
+  top(): number {
+    return this.queue[this.queue.length - 1];
+  }
+
+  empty(): boolean {
+    return this.queue.length === 0;
+  }
+}
+
+/**
+ * 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+ * 两个队列
+ */
+export class MyStack2 {
   private queue: number[] = [];
   private _queue: number[] = [];
 
