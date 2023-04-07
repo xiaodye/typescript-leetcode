f12f18426d568a1d2df71cb42e69e955b8092ac7
lansen <bluewoods.lan@gmail.com>
2023-04-07 21:48:37 +0800

    feat: 规范命名

--- src/动态规划/0-1背包问题.ts
+++ src/动态规划/0-1背包问题.ts
@@ -1,12 +1,15 @@
 /**
  * 0-1背包问题
- * @param w 物品的质量数组
- * @param v 物品的价值数组
+ * 有n件物品和一个最多能背重量为w 的背包。第i件物品的重量是weight[i]，得到的价值是value[i] 。
+ * 每件物品只能用一次，求解将哪些物品装入背包里物品价值总和最大。
+ *
+ * @param weight 物品的质量数组
+ * @param value 物品的价值数组
  * @param c 背包的容量
  * @returns 背包的最大价值
  */
-export default function knapsack(w: number[], v: number[], c: number): number {
-  const num = w.length;
+export default function knapsack(weight: number[], value: number[], c: number): number {
+  const num = weight.length;
 
   /**
    * num就是物品的数量
@@ -15,14 +18,14 @@ export default function knapsack(w: number[], v: number[], c: number): number {
   const dp = Array.from({ length: num }, () => new Array<number>(c + 1).fill(0));
 
   // 初始化首行的数据
-  for (let i = w[0]; i <= c; i++) {
-    dp[0][i] = v[0];
+  for (let i = weight[0]; i <= c; i++) {
+    dp[0][i] = value[0];
   }
 
   // 填充dp数组，此时第首行和第首列已经确立
   for (let i = 1; i < num; i++) {
     for (let j = 1; j <= c; j++) {
-      if (j < w[i]) {
+      if (j < weight[i]) {
         /**
          * 当前背包的容量都没有当前物品i大的时候，是不放物品i的
          * 那么前i-1个物品能放下的最大价值就是当前情况的最大价值
@@ -36,7 +39,7 @@ export default function knapsack(w: number[], v: number[], c: number): number {
          *    2.放物品i
          * 比较这两种情况下，哪种背包中物品的最大价值最大
          */
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
       }
     }
   }
--- src/动态规划/322. 零钱兑换.ts
+++ src/动态规划/322. 零钱兑换.ts
@@ -5,28 +5,28 @@
  * @returns
  */
 export default function coinChnage(coins: number[], amount: number): number {
-  const f: number[] = [];
-  f[0] = 0;
+  const dp: number[] = [];
+  dp[0] = 0;
 
   // i代表硬币总金额，j代表硬币种类数组的索引
   for (let i = 1; i <= amount; i++) {
-    f[i] = Infinity;
+    dp[i] = Infinity;
 
     for (let j = 0; j < coins.length; j++) {
       // 若硬币面额小于目标总额，则问题成立
       if (i - coins[j] >= 0) {
         // 状态转移方程
-        f[i] = Math.min(f[i], f[i - coins[j]] + 1);
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
       }
     }
   }
 
   // 若目标总额对应的解为无穷大，则意味着没有一个符合条件的硬币总数来更新它，本题无解，返回-1
-  if (f[amount] === Infinity) {
+  if (dp[amount] === Infinity) {
     return -1;
   }
 
-  return f[amount];
+  return dp[amount];
 }
 
 // test
