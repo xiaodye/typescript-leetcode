43a1abebfeed4936b0225f2ad38fcbaf010d7578
lansen <bluewoods.lan@gmail.com>
2023-04-16 21:28:03 +0800

    fix：添加描述

--- src/回溯/90. 子集 II.ts
+++ src/回溯/90. 子集 II.ts
@@ -1,3 +1,9 @@
+/**
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ * @param nums
+ * @returns
+ */
 export default function subsetsWithDup(nums: number[]): number[][] {
   const res: number[][] = [];
   const subset: number[] = [];
