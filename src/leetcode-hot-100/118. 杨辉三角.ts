ade0c195e16778bdf5870a5b3147ffce0f61b1bc
lansen <bluewoods.lan@gmail.com>
2023-04-23 20:34:42 +0800

    leetcode-hot-100: 118

---
+++ src/leetcode-hot-100/118. 杨辉三角.ts
@@ -0,0 +1,25 @@
+/**
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * @param numRows
+ * @returns
+ */
+export default function generate(numRows: number): number[][] {
+  const dp = Array.from({ length: numRows + 1 }, () => new Array<number>(numRows + 1).fill(0));
+  const res: number[][] = [];
+
+  dp[0][1] = 1;
+  res.push([1]);
+
+  for (let i = 1; i < numRows; i++) {
+    const arr: number[] = [];
+    for (let j = 1; j < i + 2; j++) {
+      dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+      arr.push(dp[i][j]);
+    }
+
+    res.push(arr);
+  }
+
+  return res;
+}
