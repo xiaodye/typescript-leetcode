6f8dce0eb6fa5471306dd1ea15e23f9d38557719
lansen <bluewoods.lan@gmail.com>
2023-04-28 10:57:57 +0800

    leetcode-hot-100: 73, 48

---
+++ src/leetcode-hot-100/73. 矩阵置零.ts
@@ -0,0 +1,28 @@
+/**
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ * @param matrix
+ */
+export default function setZeroes(matrix: number[][]): void {
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  const row = new Array<boolean>(m).fill(false);
+  const col = new Array<boolean>(n).fill(false);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        row[i] = true;
+        col[j] = true;
+      }
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (row[i] || col[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+}
