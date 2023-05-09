eac2df74eade5773d9cb29e921da667acd816111
lansen <bluewoods.lan@gmail.com>
2023-05-09 19:55:03 +0800

    剑指 Offer 20. 表示数值的字符串

---
+++ src/剑指 Offer/剑指 Offer 20. 表示数值的字符串.ts
@@ -0,0 +1,45 @@
+/**
+ * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+ * @param s
+ * @returns
+ */
+export default function isNumber(s: string): boolean {
+  const states = [
+    { " ": 0, s: 1, d: 2, ".": 4 },
+    { d: 2, ".": 4 },
+    { ".": 3, d: 2, e: 5, " ": 8 },
+    { d: 3, e: 5, " ": 8 },
+    { d: 3 },
+    { s: 6, d: 7 },
+    { d: 7 },
+    { d: 7, " ": 8 },
+    { " ": 8 },
+  ];
+
+  let p = 0;
+  let t = "";
+
+  type charType = "." | "d" | "e" | "s" | " ";
+
+  for (const char of s) {
+    if (/^[0-9]$/.test(char)) {
+      t = "d";
+    } else if (char === "+" || char === "-") {
+      t = "s";
+    } else if (char === "e" || char === "E") {
+      t = "e";
+    } else if (char === "." || char === " ") {
+      t = char;
+    } else {
+      t = "?";
+    }
+
+    if (t in states[p]) {
+      p = states[p][t as charType];
+    } else {
+      return false;
+    }
+  }
+
+  return [2, 3, 7, 8].includes(p);
+}
