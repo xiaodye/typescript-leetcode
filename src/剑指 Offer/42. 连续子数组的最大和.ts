4f77b5d28b33e9d275046b853d8481bd4a1cc502
lansen <bluewoods.lan@gmail.com>
2023-05-20 15:12:09 +0800

    剑指 Offer: 42

---
+++ src/剑指 Offer/42. 连续子数组的最大和.ts
@@ -0,0 +1,19 @@
+/**
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * 要求时间复杂度为O(n)。
+ * @param nums
+ * @returns
+ */
+export default function maxSubArray(nums: number[]): number {
+  const dp: number[] = [];
+  dp[0] = nums[0];
+  let maxSum = dp[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+
+    maxSum = dp[i] > maxSum ? dp[i] : maxSum;
+  }
+
+  return maxSum;
+}
