0bbb3937367699f308950db4d1896524acaf9a55
lansen <bluewoods.lan@gmail.com>
2023-05-22 00:26:28 +0800

    剑指 Offer: 40, 61

---
+++ src/剑指 Offer/排序/40. 最小的k个数.ts
@@ -0,0 +1,17 @@
+/**
+ * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ * @param arr
+ * @param k
+ * @returns
+ */
+export default function getLeastNumbers(arr: number[], k: number): number[] {
+  arr.sort((a, b) => a - b);
+
+  const res: number[] = [];
+
+  for (let i = 0; i < k; i++) {
+    res.push(arr[i]);
+  }
+
+  return res;
+}
---
+++ src/剑指 Offer/排序/61. 扑克牌中的顺子.ts
@@ -0,0 +1,28 @@
+/**
+ * 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+ * 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+ * @param nums
+ * @returns
+ */
+export default function isStraight(nums: number[]): boolean {
+  // key: 数字, 出现的次数不能超过0
+  const map = new Set<number>();
+
+  // 记录5张牌的最大和最小值
+  let max = 0;
+  let min = 14;
+
+  // 遍历数组，记录每个数字出现的次数（除了0）
+  for (const num of nums) {
+    if (num === 0) continue;
+
+    max = Math.max(max, num);
+    min = Math.min(min, num);
+
+    // 若发现有重复的，提前返回false
+    if (map.has(num)) return false;
+    map.add(num);
+  }
+
+  return max - min < 5;
+}
