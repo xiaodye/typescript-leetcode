b260ed0bc133e4263d4def995c45d064921c6111
lansen <bluewoods.lan@gmail.com>
2023-05-28 23:22:01 +0800

    feat: 更新解法

--- src/链表/138. 复制带随机指针的链表.ts
+++ src/链表/138. 复制带随机指针的链表.ts
@@ -8,33 +8,32 @@ import Node from "../data-structure/Node";
 export default function copyRandomList(head: Node | null): Node | null {
   if (!head) return null;
 
-  // 初始化头结点，map
-  const copyHead = new Node(head.val);
-  const hashMap = new Map<Node, Node>();
+  // 建立映射，源节点 -> 新节点
+  const map = new Map<Node, Node>();
 
-  // 初始化游标节点
-  let copyNode: Node | null = copyHead;
-  let curr: Node | null = head;
+  // 用于遍历的指针
+  let curr = head;
 
-  // 首次循环，正常处理链表的复制
-  while (curr && copyNode) {
-    copyNode.val = curr.val;
-    copyNode.next = curr.next ? new Node(curr.next.val) : null;
-    hashMap.set(curr, copyNode);
+  // 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+  while (curr) {
+    map.set(curr, new Node(curr.val));
     curr = curr.next;
-    copyNode = copyNode.next;
   }
 
-  // 游标复位
-  copyNode = copyHead;
+  // 指针重置
   curr = head;
 
-  // 第二次循环，特殊处理random关系
-  while (curr && copyNode) {
-    copyNode.random = curr.random ? hashMap.get(curr.random)! : null;
+  // 构建新链表的 next 和 random 指向
+  while (curr) {
+    // 复制节点之间的前后关系, 注意最后一个节点
+    map.get(curr).next = map.get(curr.next) ?? null;
+
+    // 复制随机指针
+    map.get(curr).random = map.get(curr.random);
+
     curr = curr.next;
-    copyNode = copyNode.next;
   }
 
-  return copyHead;
+  // 返回新链表的头节点
+  return map.get(head);
 }
