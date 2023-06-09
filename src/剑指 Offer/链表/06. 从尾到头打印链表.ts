1abfe027c54a7f2f44b249d38cc072a038dfea56
lansen <bluewoods.lan@gmail.com>
2023-06-09 23:47:03 +0800

    剑指 Offer: 链表

--- src/剑指 Offer/链表/06. 从尾到头打印链表.ts
+++ src/剑指 Offer/链表/06. 从尾到头打印链表.ts
@@ -1,4 +1,4 @@
-import ListNode from "../data-structure/ListNode";
+import ListNode from "../../data-structure/ListNode";
 
 /**
  * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
--- src/剑指 Offer/链表/24. 反转链表.ts
+++ src/剑指 Offer/链表/24. 反转链表.ts
@@ -1,4 +1,4 @@
-import ListNode from "../data-structure/ListNode";
+import ListNode from "../../data-structure/ListNode";
 
 /**
  * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
