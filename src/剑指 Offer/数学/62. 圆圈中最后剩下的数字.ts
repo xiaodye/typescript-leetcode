be21aa7505cefd109f7fc695ce0791d6c286c9e0
lansen <bluewoods.lan@gmail.com>
2023-06-17 14:20:59 +0800

    剑指 Offer: 62

---
+++ src/剑指 Offer/数学/62. 圆圈中最后剩下的数字.ts
@@ -0,0 +1,17 @@
+/**
+ * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。
+ * 求出这个圆圈里剩下的最后一个数字。
+ * @param n
+ * @param m
+ */
+export default function lastRemaining(n: number, m: number): number {
+  // 最终活下来那个人的初始位置
+  let pos = 0;
+
+  for (let i = 2; i <= n; i++) {
+    // 每次循环右移
+    pos = (pos + m) % i;
+  }
+
+  return pos;
+}
