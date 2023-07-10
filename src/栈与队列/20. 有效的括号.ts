0aa9769438967378e5cc4d857b8ff8863eb437bb
lansen <bluewoods.lan@gmail.com>
2023-07-10 23:07:54 +0800

    feat: update solution

--- src/栈与队列/20. 有效的括号.ts
+++ src/栈与队列/20. 有效的括号.ts
@@ -4,7 +4,7 @@
  * @returns
  */
 export default function isValid(s: string): boolean {
-  const map = new Map([
+  const map = new Map<string, string>([
     ["(", ")"],
     ["[", "]"],
     ["{", "}"],
