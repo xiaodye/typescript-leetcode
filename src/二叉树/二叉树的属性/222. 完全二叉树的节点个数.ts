e93dd1132e0368227637affb54e258fbc5db03ef
lansen <bluewoods.lan@gmail.com>
2023-07-11 23:23:09 +0800

    fix: 修复解法

--- src/二叉树/二叉树的属性/222. 完全二叉树的节点个数.ts
+++ src/二叉树/二叉树的属性/222. 完全二叉树的节点个数.ts
@@ -12,20 +12,8 @@ import TreeNode from "../../data-structure/TreeNode";
 export default function countNodes(root: TreeNode | null): number {
   if (!root) return 0;
 
-  const queue: TreeNode[] = [root];
-  let num = 0;
+  const left = countNodes(root.left);
+  const right = countNodes(root.right);
 
-  while (queue.length !== 0) {
-    const len = queue.length;
-
-    for (let i = 0; i < len; i++) {
-      const node = queue.shift();
-      num++;
-
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-  }
-
-  return num;
+  return left + right + 1;
 }
