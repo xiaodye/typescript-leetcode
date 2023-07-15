89b3be7e4df384ef10207ef53e0c28b26925b5b0
lansen <bluewoods.lan@gmail.com>
2023-07-15 13:17:31 +0800

    feat: add docs

--- src/动态规划/子序列问题/300. 最长递增子序列.ts
+++ src/动态规划/子序列问题/300. 最长递增子序列.ts
@@ -4,29 +4,25 @@
  * @returns
  */
 export default function lengthOfLIS(nums: number[]): number {
-  if (nums.length === 0) return 0;
+  if (nums.length <= 1) return nums.length;
 
+  // dp[i]: 下标 i 结尾的最长递增子序列
+  // 递推式：dp[i] = Math.max(dp[0], dp[1], dp[2], ....) + 1 (nums[j] < nums[i]; j < i)
+  // 初始化dp: 每项初始值均为 1
   const dp = new Array<number>(nums.length).fill(1);
+
+  // 用于记录最大值
   let maxLen = 1;
 
-  // 从第2个元素开始，遍历整个数组
   for (let i = 1; i < nums.length; i++) {
-    // 每遍历一个新元素，都要“回头看”，看看能不能延长原有的上升子序列
     for (let j = 0; j < i; j++) {
-      // 若遇到了一个比当前元素小的值，则意味着遇到了一个可以延长的上升子序列，故更新当前元素索引位对应的状态
-      if (nums[j] < nums[i]) {
+      if (nums[i] > nums[j]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
 
-    if (dp[i] > maxLen) {
-      maxLen = dp[i];
-    }
+    maxLen = Math.max(maxLen, dp[i]);
   }
 
   return maxLen;
 }
-
-// test
-console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
-debugger;
