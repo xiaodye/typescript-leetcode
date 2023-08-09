020dccdefc4c999d22e25069906de0c7e21b5de8
lansen <bluewoods.lan@gmail.com>
2023-08-09 23:47:28 +0800

    feat: 100, 518

---
+++ src/其他/100. 相同的树.ts
@@ -0,0 +1,20 @@
+import TreeNode from "../data-structure/TreeNode";
+
+/**
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * @param p
+ * @param q
+ * @returns
+ */
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  // p === null && q === null true
+  // p === null || q === null false
+  // p.val !== q.val false
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+
+  // 递归子节点
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
---
+++ src/动态规划/完全背包/518. 零钱兑换 II.ts
@@ -0,0 +1,26 @@
+/**
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * 假设每一种面额的硬币有无限个。
+ * 题目数据保证结果符合 32 位带符号整数。
+ * @param amount
+ * @param coins
+ */
+export default function change(amount: number, coins: number[]): number {
+  // dp[i] 表示金额之和等于 i 的硬币组合数
+  // dp[i] = sum(dp[i- coins[0]], dp[i - coins[1],...])
+  // dp[0] = 1; 金额为0 ，就是不放硬币 1种
+
+  const dp = new Array<number>(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 1; j <= amount; j++) {
+      if (j >= coins[i]) {
+        dp[j] += dp[j - coins[i]];
+      }
+    }
+  }
+
+  return dp[amount];
+}
