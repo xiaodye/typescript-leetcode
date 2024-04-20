109fa464baea5002a35da92d942769e41c27f78c
lansen <bluewoods.lan@gmail.com>
2024-04-20 13:46:53 +0800

    test: 新增单测

--- src/哈希表/383. 赎金信.ts
+++ src/哈希表/383. 赎金信.ts
@@ -1,8 +1,6 @@
 /**
  * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
- *
  * 如果可以，返回 true ；否则返回 false 。
- *
  * magazine 中的每个字符只能在 ransomNote 中使用一次。
  *
  * - 时间复杂度：O(m + n)
@@ -12,7 +10,12 @@
  * @param magazine
  * @returns
  */
-export default function canConstruct(ransomNote: string, magazine: string): boolean {
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    // 思路：
+    // 1.首先判断字母长度是否符合
+    // 2. 遍历 magazine, 记录每个字母的个数和种类
+    // 3. 遍历 ransomNote
+
     if (ransomNote.length > magazine.length) return false;
 
     // key: 字符, value: 个数
@@ -27,15 +30,15 @@ export default function canConstruct(ransomNote: string, magazine: string): bool
         }
     }
 
-    for (let i = 0; i < ransomNote.length; i++) {
-        const char = ransomNote[i];
-
-        if (!visted.has(char) || visted.get(char) === 0) {
-            return false;
-        }
+    for (const char of ransomNote) {
+        if (!visted.has(char)) return false;
 
         visted.set(char, visted.get(char) - 1);
+
+        if (visted.get(char) < 0) return false;
     }
 
     return true;
 }
+
+export default canConstruct;
--- src/哈希表/__test__/basic.test.ts
+++ src/哈希表/__test__/basic.test.ts
@@ -1,6 +1,7 @@
 import isAnagram from "../242. 有效的字母异位词";
 import intersection from "../349. 两个数组的交集";
 import isHappy from "../202. 快乐数";
+import canConstruct from "../383. 赎金信";
 
 describe("242. 有效的字母异位词", () => {
     it("test-1", () => {
@@ -21,5 +22,13 @@ describe("202. 快乐数", () => {
     it("test-1", () => {
         expect(isHappy(19)).toBeTruthy();
         expect(isHappy(2)).toBeFalsy();
+    });
+});
+
+describe("383. 赎金信", () => {
+    it("test-1", () => {
+        expect(canConstruct("aa", "aab")).toBeTruthy();
+        expect(canConstruct("a", "b")).toBeFalsy();
+        expect(canConstruct("aa", "ab")).toBeFalsy();
     });
 });
